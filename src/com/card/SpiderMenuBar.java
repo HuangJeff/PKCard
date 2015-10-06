ed35d01b1b779b393e7832e28aa285ecd6013c75
huangjeff <1qaz@2wsx>
2015-10-06 22:24:12 +0800

    新添加一支程式

---
+++ src/com/card/SpiderMenuBar.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.card;
+
+import javax.swing.JMenuBar;
+
+/**
+ * 生成遊戲的功能表欄
+ * @author jeff
+ * @date 2015/10/06
+ */
+public class SpiderMenuBar extends JMenuBar {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public SpiderMenuBar() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
