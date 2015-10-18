a7c81c7ac66cf50d469e6bfe28ab9c1a6e8ad5d7
huangjeff <1qaz@2wsx>
2015-10-18 22:22:09 +0800

    PKCard 圖片顯示

--- src/com/card/PKCard.java
+++ src/com/card/PKCard.java
@@ -3,10 +3,15 @@
  */
 package com.card;
 
+import java.awt.Container;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 /**
@@ -15,13 +20,87 @@ import javax.swing.JLabel;
  * @date 2015/10/09
  */
 public class PKCard extends JLabel implements MouseListener, MouseMotionListener {
-
 	/**
 	 * 
 	 */
-	public PKCard() {
+	private static final long serialVersionUID = -1685847849234515380L;
+	//紙牌的位置
+	private Point point = null;
+	/** 紙牌最後位置 */
+	private Point initPoint = null;
+	
+	private int value = 0;
+	private int type = 0;
+	
+	private String name = null;
+	
+	private PKCard previousCard = null;
+	private Container pane = null;
+	private Spider main = null;
+	
+	private boolean canMove = false;
+	/** 是否為正面 */
+	private boolean isFront = false;
+	
+	/**
+	 * Constructor
+	 * @param name : 格式是[1-10,1-1,1-2...]
+	 * @param spider : Spider
+	 */
+	public PKCard(String name, Spider spider) {
+		super();
+		String[] aryOfName = name.split("-");
+		this.type = Integer.parseInt(aryOfName[0]);
+		this.value = Integer.parseInt(aryOfName[1]);
 		
+		this.name = name;
+		this.main = spider;
+		//default Picture
+		//this.setIcon(new ImageIcon("images/rear.gif")); //can't work
+		try {
+			//URL url = this.getClass().getResource("images/");	//找到(資料夾)的物件(因為要用URL，所以要加反鈄線)
+			URL url = this.getClass().getResource("../images/"); //相對路徑
+			this.setIcon(new ImageIcon(new URL(url, "rear.gif")));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		this.setSize(71, 96);
+		this.setVisible(true);
+		
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
 	}
+	
+	/**
+	 * 轉至正面
+	 */
+	public void turnFront() {
+		this.setIcon(new ImageIcon("images/" + name + ".gif"));
+		this.isFront = true;
+	}
+	
+	/**
+	 * 轉至背面
+	 */
+	public void turnRear() {
+		this.setIcon(new ImageIcon("images/rear.gif"));
+		this.isFront = false;
+		this.canMove = false;
+	}
+	
+	/**
+	 * 將牌移至point點處
+	 * @param point
+	 */
+	public void moveto(Point point) {
+		//setLocation = 將元件移到新位置。通過此元件父級坐標空間中的 x 和 y 參數來指定新位置的左上角。
+		this.setLocation(point);
+		this.initPoint = point;
+	}
+	
+	
+	
 	//----------MouseMotionListener----------
 	@Override
 	public void mouseDragged(MouseEvent e) {
--- src/com/card/Spider.java
+++ src/com/card/Spider.java
@@ -10,11 +10,15 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Hashtable;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EtchedBorder;
 
@@ -93,7 +97,16 @@ public class Spider extends JFrame {
 			 */
 			groundLabel[i].setBorder(BorderFactory.createEtchedBorder(
 					EtchedBorder.RAISED));
-			groundLabel[i].setBounds(x, 25, 71, 96);
+			//groundLabel[i].setBounds(x, 25, 71, 96);
+			groundLabel[i].setBounds(x, 25, 71, 100);
+			//For Test
+			/*try {
+				//URL url = this.getClass().getResource("images/");	//找到(資料夾)的物件(因為要用URL，所以要加反鈄線)
+				URL url = this.getClass().getResource("../images/"); //相對路徑
+				groundLabel[i].setIcon(new ImageIcon(new URL(url, "rear.gif")));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}*/
 			x += 101;
 			this.pane.add(groundLabel[i]);
 		}
@@ -155,16 +168,17 @@ public class Spider extends JFrame {
 		//初始card的值
 		for(int i=1;i<=8;i++) {	//2副牌(2 * 4種花色)
 			for(int j=1;j<=13;j++) { //13張牌
+				String _cardName = (i % _n + 1) + "-" + j; //=> new PKCard((i % n + 1) + "-" + j, this);
 				int cardNum = (i - 1) * 13 + j - 1;
+				// ex:card value i = [1] and j = [1] value = 0 _cardName = 1-1
 				//System.out.println("card value i = [" + i +
-				//		"] and j = [" + j + "] value = " + cardNum);
-				cards[cardNum] = null;
-						//new PKCard((i % n + 1) + "-" + j, this);
+				//		"] and j = [" + j + "] value = " + cardNum + " _cardName = " + _cardName);
+				cards[cardNum] = new PKCard(_cardName, this);
 			}
 		}
 		
-		//隨機紙牌初始化
-		this.randomCards();
+		//隨機紙牌初始化 TODO 先Mark，之後要開
+		//this.randomCards();
 	}
 	
 	/**
@@ -199,21 +213,21 @@ public class Spider extends JFrame {
 		int x = 883;
 		int y = 580;
 		//初始化待展開的紙牌(共60張牌，待展開)
-		for(int i=0;i<6;i++) {
+		/*for(int i=0;i<6;i++) {
 			for(int j=0;j<10;j++) {
 				int _n = i * 10 + j;
 				System.out.println("初始化待展開的紙牌 _n = " + _n);
-				//pane.add(cards[_n]);
+				this.pane.add(cards[_n]);
 				//將card轉向背面
-				
-				//將card放有固定的位置上
-				
+				cards[_n].turnRear();
+				//將card放有固定的位置上(上方白色框內)
+				cards[_n].moveto(new Point(x, y));
 				//將card的位置及相關資訊存入
-				//table.put(new Point(x, y), cards[_n]);
+				table.put(new Point(x, y), cards[_n]);
 			}
 			//x軸+10
 			x += 10;
-		}
+		}*/
 		x = 20;
 		y = 45;
 		//初始化表面顯示的紙牌
@@ -223,20 +237,91 @@ public class Spider extends JFrame {
 				if(_n >= 104)
 					continue;
 				System.out.println("初始化表面顯示的紙牌 _n = " + _n);
-				//pane.add(cards[_n]);
-				//轉面
-				
+				this.pane.add(cards[_n]);
+				//轉背面
+//				cards[_n].turnRear();
 				//移動到固定位置
-				
+				cards[_n].moveto(new Point(x, y));
 				//將card的位置及相關資訊存入
-				//table.put(new Point(x, y), cards[_n]);
+//				table.put(new Point(x, y), cards[_n]);
 				x += 101;
 			}
 			x = 20;
 			y -= 5;
 		}
+	}
+	
+	/**
+	 * 設置還原
+	 */
+	private void setNA() {
+		this.a = 0;
+		this.n = 0;
+	}
+	
+	/**
+	 * 遊戲建立
+	 */
+	private void deal() {
+		this.setNA();
+		//判斷10列中是否空列
+		for(int i=0;i<10;i++) {
+			if(this.getLastCardLocation(i) == null) {
+				JOptionPane.showMessageDialog(this,
+						"有空位不能發牌！", "提示", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+		}
+		int x = 20;
 		
-		
+		for(int i=0;i<10;) {
+			Point lastPoint = this.getLastCardLocation(i);
+			//這張牌應"背面向上"
+			if(c == 0) {
+				lastPoint.y += 5;
+			} //這張牌應"正面向上"
+			else {
+				lastPoint.y += 20;
+			}
+			
+			table.remove(cards[c + i].getLocation());
+			//cards[c + i]
+			
+		}
+	}
+	
+	/**
+	 * 取得第column列最後一張牌的位置<br>
+	 * 比對放入Hashtable[table]
+	 * @param column
+	 * @return
+	 */
+	private Point getLastCardLocation(int column) {
+		Point point = new Point(20 + column * 101, 25);
+		PKCard card = (PKCard)this.table.get(point);
+		if(card == null)
+			return null;
+		while(card != null) {
+			point = card.getLocation();
+			card = this.getNextCard(card);
+		}
+		return point;
+	}
+	
+	/**
+	 * 取得card下面的一張牌
+	 * @param card
+	 * @return
+	 */
+	private PKCard getNextCard(PKCard card) {
+		Point point = new Point(card.getLocation());
+		point.y += 5;
+		card = (PKCard)this.table.get(point);
+		if(card != null)
+			return card;
+		point.y += 15;
+		card = (PKCard)this.table.get(point);
+		return card;
 	}
 	
 	/**
